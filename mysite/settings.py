6b93c5bc1483dcadae0ba8cba08346b0d4445302
Rubens Rodrigues <rubens_v2@hotmail.com>
2017-01-13 23:45:00 -0200

    Troca de DB, de SQLite3 por Postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}"""
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'blog',
+		'USER': 'rodriguesv2',
+		'PASSWORD': 'systemof',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
